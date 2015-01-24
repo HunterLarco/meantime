704b47a1ce1f0cd1293bfdc5284c8720e4708ca3
Hunter John Larco <hjlarco@gmail.com>
2015-01-24 18:02:38 -0500

    Official Landing Page Developed

--- server/landing/app.py
+++ server/landing/app.py
@@ -10,6 +10,25 @@ class MainHandler(webapp2.RequestHandler):
     self.response.out.write(template.render(path, template_values))
 
 
+class AddEmail(webapp2.RequestHandler):
+  def get(self):
+    self.redirect('/')
+  def post(self):
+    email = self.request.get('email')
+    SendEmail(email);
+
+
+def SendEmail(email):
+  from google.appengine.api import mail
+  message = mail.EmailMessage(sender="Sealed Team <admin@trysealed.com>",
+                              subject='New Beta Tester')
+  message.to = 'hjlarco@gmail.com'
+  message.body = "Hey Alex! <%s> just signed up to become a beta tester" % email
+  message.html = message.body
+  message.send()
+
+
 app = webapp2.WSGIApplication([
+                ('/addemail/?', AddEmail),
                 ('/.*', MainHandler)
               ], debug=True)
