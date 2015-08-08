6c03b2ae298a89f83399dbba48259692747e8623
Hunter John Larco <hjlarco@gmail.com>
2015-08-08 11:21:11 -0700

    Design Update (not mobile ready)

--- server/dev/resources/Scripts/pages/header.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  
-  window.addEventListener('load', function(){
-    document.getElementById('header_capturebutton').addEventListener('click', function(){
-      app.pages['capture'].focus();
-    });
-  });
-  
-})();
